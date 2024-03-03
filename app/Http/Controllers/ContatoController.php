c008c01704a6f47ef40f4ffc4370b37c399c15d1
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-03 17:03:31 -0300

    feat: adiciona envio e validação de contato, recuperação de valor antigo e refatoração do menu de seleção do formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function index() {
-        return view('site.contato');
+    public function index(Request $request) {
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000',
+
+        ]);
+
+        SiteContato::create($request->all());
     }
 }
--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function index() {
-        return view('site.principal');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }
