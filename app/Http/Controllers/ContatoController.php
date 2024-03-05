625bbd05de48431e1a84a029d8b4e8967f8daf7f
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-05 18:26:17 -0300

    feat: adiciona validação de email

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -19,12 +19,13 @@ class ContatoController extends Controller
         $request->validate([
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000',
 
         ]);
-
         SiteContato::create($request->all());
+        return redirect()->route('site.index
+        ');
     }
 }
