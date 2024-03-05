9b77a7c9ef85e619f41a752230eb5f33547384e3
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-05 19:44:30 -0300

    feat: customiza mensagens de erro

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -16,14 +16,22 @@ class ContatoController extends Controller
     }
 
     public function store(Request $request) {
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000',
-
-        ]);
+        $request->validate(
+            [
+                'nome' => 'required|min:3|max:40',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000',
+            ],
+            [
+                'nome.min' => 'O campo nome precisa ter no mínimo 3  caracteres',
+                'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+                'email.email' => 'O email informado não é válido',
+                'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+                'required' => 'O campo :attribute deve ser preenchido'
+            ]
+        );
 
         SiteContato::create($request->all());
 
