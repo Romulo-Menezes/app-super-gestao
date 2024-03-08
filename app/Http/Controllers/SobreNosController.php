896756f7223309037e4941b816cc907d4037f0c9
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-08 18:04:19 -0300

    feat: cria middleware de log de acesso

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
+    public function __construct() {
+        $this->middleware(LogAcessoMiddleware::class);
+    }
+
     public function index() {
         return view('site.sobre-nos');
     }
