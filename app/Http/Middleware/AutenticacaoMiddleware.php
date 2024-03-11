9730df28900564ced3383d32e64befb8b8084afe
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-11 15:38:38 -0300

    feat: passagem de parâmetros para o middleware

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,8 +13,22 @@ class AutenticacaoMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $metodo_verificacao, string $perfil): Response
     {
+        echo "Método de verificação: $metodo_verificacao | Perfil: $perfil <br>";
+        if ($metodo_verificacao == 'padrao') {
+            echo 'Verificar o usuário e senha no banco de dados <br>';
+        } elseif ($metodo_verificacao == 'ldap') {
+            echo 'Verificar o usuário e senha no AD <br>';
+        }
+
+        if ($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos <br>';
+        } else {
+            echo 'Carregar perfil do banco de dados';
+        }
+
+
         if (true) {
             return $next($request);
         } else {
--- routes/web.php
+++ routes/web.php
@@ -26,19 +26,15 @@ Route::get('/contato', [ContatoController::class, 'index'])->name('site.contato'
 Route::post('/contato', [ContatoController::class, 'store'])->name('site.contato');
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
-Route::prefix('app')->group( function(){
-    Route::middleware('log.acesso', 'autenticacao')
-        ->get('/clientes', function(){ return 'clientes'; })
-        ->name('app.clientes');
-
-    Route::middleware('log.acesso', 'autenticacao')
-        ->get('/fornecedores', [FornecedorController::class, 'index'])
-        ->name('app.fornecedores');
-
-    Route::middleware('log.acesso', 'autenticacao')
-        ->get('/produtos', function(){ return 'produtos'; })
-        ->name('app.produtos'); 
-});
+Route::middleware('autenticacao:padrao,visitante')
+    ->prefix('app')
+    ->group( 
+        function(){
+            Route::get('/clientes', function(){ return 'clientes'; })->name('app.clientes');
+            Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+            Route::get('/produtos', function(){ return 'produtos'; })->name('app.produtos'); 
+        }
+    );
 
 Route::fallback(function(){
     echo '<h3>A rota acessada não existe! <a href="'.route('site.index').'">clique aqui</a> para voltar à página inicial.</h3>';
