8a23172ad94093a0e0dd7031077cbfd4e54f2ec1
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-11 14:08:46 -0300

    feat: apelida middleware

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 class SobreNosController extends Controller
 {
+    public function __construct() {
+        $this->middleware('log.acesso');
+    }
+
     public function index() {
         return view('site.sobre-nos');
     }
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index')->middleware('log.acesso');
 
 Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('site.sobreNos');
 
-Route::get('/contato', [ContatoController::class, 'index'])->name('site.contato');
+Route::get('/contato', [ContatoController::class, 'index'])->name('site.contato')->middleware('log.acesso');
     
 Route::post('/contato', [ContatoController::class, 'store'])->name('site.contato');
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
