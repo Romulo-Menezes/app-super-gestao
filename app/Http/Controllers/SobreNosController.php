4170ba33e988c03b3f20144808cb1a69ca687777
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-11 15:37:41 -0300

    refactor: remove construtor

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\Controller;
 
 class SobreNosController extends Controller
 {
-    public function __construct() {
-        $this->middleware('log.acesso');
-    }
-
     public function index() {
         return view('site.sobre-nos');
     }
