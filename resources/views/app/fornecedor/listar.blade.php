139fe949e6266d1b91a5492ae5dd046d4ace19e6
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-03-22 15:20:55 -0300

    feat: pesquisa de registros (fornecedor)

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -19,8 +19,31 @@
         </ul>
     </div>
     <div class="informacao-pagina">
-        <div style="width: 30%; margin-left: auto; margin-right: auto;">
-            ... lista ...
+        <div style="width: 90%; margin-left: auto; margin-right: auto;">
+            <table border="1" width="100%">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Site</th>
+                        <th>UF</th>
+                        <th>E-mail</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($fornecedores as $fornecedor)
+                        <tr>
+                            <th>{{ $fornecedor->nome }}</th>
+                            <th>{{ $fornecedor->site }}</th>
+                            <th>{{ $fornecedor->uf }}</th>
+                            <th>{{ $fornecedor->email }}</th>
+                            <th>Editar</th>
+                            <th>Excluir</th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
