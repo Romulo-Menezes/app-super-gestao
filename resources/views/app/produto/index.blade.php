876ba3903060a758910b11ebc245684eb6464e7f
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-07-18 17:34:41 -0300

    Estabelece relacionamento 1xN (hasMany)

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -25,6 +25,7 @@
                     <tr>
                         <th>Nome</th>
                         <th>Descrição</th>
+                        <th>Fornecedor</th>
                         <th>Peso</th>
                         <th>Unidade ID</th>
                         <th>Comprimento</th>
@@ -40,6 +41,7 @@
                         <tr>
                             <th>{{ $produto->nome }}</th>
                             <th>{{ $produto->descricao }}</th>
+                            <th>{{ $produto->fornecedor->nome }}</th>
                             <th>{{ $produto->peso }}</th>
                             <th>{{ $produto->unidade_id }}</th>
                             <th>{{ $produto->produtoDetalhe->comprimento ?? '' }}</th>
