afdc726dc1a41ab054b3d2fb33872ba6ba59e7b4
Romulo-Menezes <romulo.m.menezes@hotmail.com>
2024-07-23 10:34:21 -0300

    Adiciona controladores e rotas para clientes, pedidos e pedidos produtos

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ClienteController extends Controller
+class PedidoProdutoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('app.cliente');
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\PedidoProdutoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProdutoDetalheController;
@@ -20,18 +23,17 @@ Route::get('/', 'App\Http\Controllers\PrincipalController@index')->name('site.in
 Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@index')->name('site.sobreNos');
 
 Route::get('/contato', 'App\Http\Controllers\ContatoController@index')->name('site.contato')->middleware('log.acesso');
-    
+
 Route::post('/contato', 'App\Http\Controllers\ContatoController@store')->name('site.contato');
 Route::get('/login/{erro?}', 'App\Http\Controllers\LoginController@index')->name('site.login');
-Route::post('/login','App\Http\Controllers\LoginController@autenticar')->name('site.login');
+Route::post('/login', 'App\Http\Controllers\LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')
     ->prefix('app')
-    ->group( 
-        function(){
+    ->group(
+        function () {
             Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('app.home');
             Route::get('/sair', 'App\Http\Controllers\LoginController@sair')->name('app.sair');
-            Route::get('/cliente', 'App\Http\Controllers\ClienteController@index')->name('app.cliente');
 
             Route::get('/fornecedor', 'App\Http\Controllers\FornecedorController@index')->name('app.fornecedor');
             Route::get('/fornecedor/adicionar', 'App\Http\Controllers\FornecedorController@adicionar')->name('app.fornecedor.adicionar');
@@ -43,9 +45,12 @@ Route::middleware('autenticacao:padrao,visitante')
 
             Route::resource('produto', ProdutoController::class);
             Route::resource('produto-detalhe', ProdutoDetalheController::class);
+            Route::resource('cliente', ClienteController::class);
+            Route::resource('pedido', PedidoController::class);
+            Route::resource('pedido-produto', PedidoProdutoController::class);
         }
     );
 
-Route::fallback(function(){
-    echo '<h3>A rota acessada não existe! <a href="'.route('site.index').'">clique aqui</a> para voltar à página inicial.</h3>';
+Route::fallback(function () {
+    echo '<h3>A rota acessada não existe! <a href="' . route('site.index') . '">clique aqui</a> para voltar à página inicial.</h3>';
 });
